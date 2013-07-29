3fed0e1571b18efe5e55bc7488b984dcb7ba6396
kumaranm <kumaranhere@gmail.com>
2013-07-29 22:49:51 +0530

    created new DisplayMessageActivity. not yet complete

--- MyFirstApp/src/com/mk/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/mk/example/myfirstapp/MainActivity.java
@@ -1,23 +1,39 @@
 package com.mk.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public static final String EXTRA_MESSAGE = "com.mk.example.myfirstapp.EXAMPLE";
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * Called when user clicks send button
+	 * 
+	 * @param view
+	 */
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText et = (EditText) findViewById(R.id.edit_message);
+		String message = et.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
 }
